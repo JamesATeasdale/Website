e8839dc0a41be6e019c27a92e87f9cda00326b86
JamesATeasdale <jamesadamteasdale@gmail.com>
2023-01-26 16:39:01 +0000

    Add files via upload

---
+++ components.js
@@ -0,0 +1,167 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+            <a href="index.html"><img class="logo" src="resources/logo.png"></a>
+                <ul class="nav_links">
+                    <li><a href="video.html">Video</a></li>
+                    <li class="dropdown">Projects
+                        <div class="dropdown-content">
+                            <a href="#home">Adventure</a>
+                            <a href="#scraper">Scraper</a>
+                            <a href="#chat">Chatbot</a>
+                            <a href="spreadsheet.html">Calorie Calc</a>
+                        </div>
+                    </li>
+                    <li class="dropdown"><a href="jsgames.html">Games</a>
+                        <div class="dropdown-content">
+                            <a href="mechAliens.html">Mech Aliens</a>
+                        </div>
+                    </li>
+                    <li><a href="questionnaire.php">Questionnaire</a></li>
+                </ul>
+            <div id="sidenav" class="sidenav">
+                <div class="profile">
+                    <img src="resources/profile.png" alt="profile_photo">
+                    <h3>James Teasdale</h3>
+                    <p>Enthusiest</p>
+                </div>
+                <div class="items">
+                    <a href="https://www.codecademy.com/profiles/jamesTeasdale9097991433">Codecademy</a>
+                    <a href="https://www.youtube.com/channel/UCcu0L3IHVaEGfBcN7tUhATw">YouTube</a>
+                    <a href="https://github.com/JamesATeasdale">Github</a>
+                    <a href="https://www.twitter.com/ShyGuro">Twitter</a>
+                    <a href="mailto:jamesadamteasdale@gmail.com">Email Me!</a>
+                    <a href="#">Server</a>
+                    <a href="smb://james@192.168.1.94/James/server/coding/html_playground/resources/james_teasdale.pdf" download="james_teasdale.pdf">CV</a>
+                </div>
+            </div>
+            <div id="main">
+                    <span onclick="toggleNav();">
+                        <div class="container" onclick="navAnim(this)">
+                            <div class="bar1"></div>
+                            <div class="bar2"></div>
+                            <div class="bar3"></div>
+                        </div>
+                    </span>
+            </div>
+      </header>
+      <div class="social">
+        <a href="https://www.twitter.com/ShyGuro"><img  src="./resources/twitter.png"></a>
+        <a href="https://www.youtube.com/channel/UCcu0L3IHVaEGfBcN7tUhATw"><img src="./resources/youtube.png"></a>
+        <a href="https://www.facebook.com/ishykel/"><img  src="./resources/facebook.png"></a>
+      </div>
+      `;
+  }
+}
+customElements.define('header-component', Header);
+
+
+/* Navigation button in the top-right */
+
+function toggleNav() {
+	var sidenav = document.getElementById("sidenav"),
+    main = document.getElementById("main");
+    sidenav.style.width = sidenav.style.width == "250px" ? '0' : '250px';
+}
+
+function navAnim(x) {
+    x.classList.toggle("change");
+}
+
+/* Choose random video on webm.html */
+
+function randomVid() {
+    var video = document.getElementById('video');
+    var number = Math.floor(Math.random() * 3) + 1;
+    video.src = "webms/" + number +'.webm#t=0.1';
+}
+
+/* For RPS game https://www.youtube.com/watch?v=1yS-JV4fWqY&list=LL&index=2 */
+
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const wins = document.getElementById('wins')
+const SELECTIONS = [{
+    name: 'rock',
+    source: 'resources/rock.png',
+    beats: 'scissors'
+}, {
+    name: 'paper',
+    source: 'resources/paper.png',
+    beats: 'rock'
+}, {
+    name: 'scissors',
+    source: 'resources/scissors.png',
+    beats: 'paper'
+}]
+ 
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+        if ((parseInt(yourScoreSpan.innerText) === 3) || (parseInt(computerScoreSpan.innerText) === 3)) {
+            wins.style.visibility = "visible";
+            if (parseInt(yourScoreSpan.innerText) > parseInt(computerScoreSpan.innerText)) {
+                wins.innerHTML = "You WIN!"
+            } else {
+                wins.innerHTML = "Comp WIN!"
+            }
+        };
+    });
+})
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const youWin = isWinner(selection, computerSelection)
+    const computerWin = isWinner(computerSelection, selection)
+    
+    addSelectionResult(computerSelection, computerWin)
+    addSelectionResult(selection, youWin)
+
+    if ((parseInt(yourScoreSpan.innerText) === 3) || (parseInt(computerScoreSpan.innerText) === 3)) {
+        wins.innerHTML = "";
+        wins.style.visibility = "hidden";
+        yourScoreSpan.innerText = 0;
+        computerScoreSpan.innerText = 0;
+        var child = document.querySelectorAll('.result-selection');
+        child.forEach(elem => {
+            elem.remove()
+        });
+    } else {
+        if (youWin) incrementScore(yourScoreSpan)
+        if (computerWin) incrementScore(computerScoreSpan) 
+    }
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+    var img = document.createElement('img')
+    const div = document.createElement('div').appendChild(img)
+    div.classList.add('result-selection')
+    img.src = selection.source
+    /* img.opacity -= 0.1; */
+
+    document.getElementById('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+} 
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
---
+++ questionnaire.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>James Teasdale</title>
+  <meta name="description" content="My first page">
+  <link rel="stylesheet" href="jamessite.css">
+  <script src="components.js" type="text/javascript" defer></script>
+</head>
+<header-component></header-component>
+<body>
+<?php
+/* POST gives annoying popups, so I used GET instead */
+if(isset($_GET['submit'])) {
+    $name = "Name: ".$_GET['name']."\n";
+    $email = "Email: ".$_GET['email']."\n";
+    $passwd = "Password: ".$_GET['passwd']."\n";
+    $browser = "Browser: ".$_GET['browser']."\n";
+    $gender = "Gender: ".$_GET['gender']."\n";
+    $feedback = "Feedback: ".$_GET['feedback']."\n";
+    $posi = "Positive Feedback: ".$_GET['posi']."\n";
+    $nega = "Negative Feedback: ".$_GET['nega']."\n\n";
+
+$file=fopen("feedback.txt", "a");
+fwrite($file, $name);
+fwrite($file, $email);
+fwrite($file, $passwd);
+fwrite($file, $browser);
+fwrite($file, $gender);
+fwrite($file, $feedback);
+fwrite($file, $posi);
+fwrite($file, $nega);
+fclose($file);
+}
+?>
+    <div class="welcome">
+        <h1>Welcome to our survey!</h1>
+        <p>We're looking forward to getting your answers so we can make sure our products and services are the best they can be!</p>
+    </div>
+    <form method="get" enctype="multipart/form-data" id="form" autocomplete="off">
+    <table class="forminfo" td align="center">
+      <tr>
+        <th>Name: </th> 
+        <td align="right"><input name="name"></td>
+      </tr>
+      <tr>
+        <th>Email Address: </th> 
+        <td align="right"><input name="email"></td>
+      </tr>
+      <tr>
+        <th>Password: </th>
+        <td align="right"><input name="passwd" placeholder="Use Caps and numbers"></td>
+      </tr>
+      <tr>
+        <th>Browser: </th>
+        <td align="right">
+          <input list="browser" name="browser">
+          <datalist>
+            <option value="Chrome">
+            <option value="Firefox">
+            <option value="Brave">
+            <option value="Chromium">
+            <option value="Opera">
+          </datalist>
+        </td>
+      </tr>
+      <tr>
+        <th>Gender</th>
+        <td align="right">
+          <select name="gender">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+            <option value="Prefer not to say">Prefer not to say</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <th>Do you like this website?</th>
+        <td align="right" >
+          <input type="radio" value="yes" name="feedback"/>Yes
+          <input type="radio" value="no" name="feedback"/>No
+        </td>
+      </tr>
+      <tr>
+        <th colspan="2">What you like about my website: </th>
+      </tr>
+      <td colspan="2">
+      <textarea cols="50" rows="5" name="posi"></textarea>
+      </td>
+      <tr>
+        <th colspan="2">What you would change about this website: </th>
+      </tr>
+      <td colspan="2">
+        <textarea cols="50" rows="5" name="nega"></textarea>
+        
+      </td>
+
+
+  </table>
+  <input class="button" type="submit" name="submit"/>
+</form>
+
+ 
+        <!---
+        <div class="question">
+          <h4>Question 2</h4>
+          <h2>I try to keep up to date with the latest fashion in active wear.</h2>
+      
+          <div class="answer">
+            <h3>Disagree</h3>
+          </div>
+      
+          <div class="answer">
+            <h3>Neutral</h3>
+          </div>
+      
+          <div class="answer">
+            <h3>Agree</h3>
+          </div>
+      </div>
+    
+      <div class="question">
+        <h4>Question 2</h4>
+        <h2>I try to keep up to date with the latest fashion in active wear.</h2>
+    
+        <div class="answer">
+          <h3>Disagree</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Neutral</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Agree</h3>
+        </div>
+      </div>
+    
+      <div class="question">
+        <h4>Question 3</h4>
+        <h2>I purchase clothing online regularly.</h2>
+    
+        <div class="answer">
+          <h3>Disagree</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Neutral</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Agree</h3>
+        </div>
+      </div>
+    
+      <div class="question">
+        <h4>Question 4</h4>
+        <h2>I try to buy goods that are designed and/or manufactured in my home country.</h2>
+    
+        <div class="answer">
+          <h3>Disagree</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Neutral</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Agree</h3>
+        </div>
+      </div>
+    
+      <div class="question">
+        <h4>Question 5</h4>
+        <h2>I look to famous athletes when trying to choose what to wear when training.</h2>
+    
+        <div class="answer">
+          <h3>Disagree</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Neutral</h3>
+        </div>
+    
+        <div class="answer">
+          <h3>Agree</h3>
+        </div>
+      </div> -->
+      <footer>
+        <h3>Thanks for taking our survey!</h3>
+      </footer>
+
+</body>
+</html>
